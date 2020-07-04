2725efb5639e1deaa17352b05934e97cca2ed314
BrenoCPimenta <brenocpimenta@gmail.com>
2020-07-04 16:38:29 -0300

    Add pokerRNN modulde
    Create enviroment variables files

---
+++ pokerRNN/env.py
@@ -0,0 +1,27 @@
+import os
+
+os.environ['PROJECT_DIR'] = ""
+
+os.environ['PREFLOP'] = os.getenv('PROJECT_DIR') + ''
+os.environ['PLURIBUS'] = os.getenv('PROJECT_DIR') + ''
+os.environ['RAISE_STACK'] = os.getenv('PROJECT_DIR') + ''
+os.environ['RAISE_STACK_PREFLOP'] = os.getenv('PROJECT_DIR') + ''
+os.environ['RAISE_STACK_AFTERFLOP'] = os.getenv('PROJECT_DIR') + ''
+os.environ['RAISE_POT_PREFLOP'] = os.getenv('PROJECT_DIR') + ''
+os.environ['RAISE_POT_AFTERFLOP'] = os.getenv('PROJECT_DIR') + ''
+
+os.environ['CONCATED_HANDS'] = os.getenv('PROJECT_DIR') + ''
+
+os.environ['HANDIDS_PER_SITUATIONS_PREFLOP'] = os.getenv('PROJECT_DIR') + ''
+os.environ['HANDIDS_PER_SITUATIONS_FLOP'] = os.getenv('PROJECT_DIR') + ''
+os.environ['HANDIDS_PER_SITUATIONS_TURN'] = os.getenv('PROJECT_DIR') + ''
+os.environ['HANDIDS_PER_SITUATIONS_RIVER'] = os.getenv('PROJECT_DIR') + ''
+
+
+os.environ['NOT_NORMALIZED_TRAIN'] = os.getenv('PROJECT_DIR') + ''
+os.environ['NOT_NORMALIZED_TEST'] = os.getenv('PROJECT_DIR') + ''
+
+
+os.environ['SCALER'] = os.getenv('PROJECT_DIR') + ''
+os.environ['TRAIN'] = os.getenv('PROJECT_DIR') + ''
+os.environ['TEST'] = os.getenv('PROJECT_DIR') + ''
